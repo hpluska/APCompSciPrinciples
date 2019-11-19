f6c47d263e3903f15fd3a1a1b2c2837e2040c8f3
hpluska <17304761+hpluska@users.noreply.github.com>
2019-11-19 11:06:16 -0700

    adds

--- labs/JavaScript/DocumentObjectModel/Stubs/App.js
+++ labs/JavaScript/DocumentObjectModel/Stubs/App.js
@@ -1,5 +1,7 @@
 //prompts the user for a background color and assigns it to the background
 var b = prompt("What color would you like to make the background?\nPlease enter the hex value.");
+
+document.getElementById("foodLogoImage").style.border = c;
 var bg = document.body;
 bg.style.backgroundColor = b;
 
