07a320c076b0247a40a0a323d3a97b58d9830270
hpluska <17304761+hpluska@users.noreply.github.com>
2019-11-20 12:06:36 -0700

    edits

--- labs/JavaScript/DocumentObjectModel/Stubs/App.js
+++ labs/JavaScript/DocumentObjectModel/Stubs/App.js
@@ -1,5 +1,5 @@
 //prompts the user for a background color and assigns it to the background
-var a = prompt("What color would you like to make the background?\nPlease enter the hex value.");
+var a = prompt("What color would you like to make the background?");
 var a2 = document.body;
 a2.style.backgroundColor = a;
 
