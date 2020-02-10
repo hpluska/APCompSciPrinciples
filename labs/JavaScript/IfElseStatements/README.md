29c9f84715c6c9d58ea5f122c64289f6a590b71f
hpluska <17304761+hpluska@users.noreply.github.com>
2020-02-10 07:12:13 -0700

    added labs

---
+++ labs/JavaScript/IfElseStatements/README.md
@@ -0,0 +1,40 @@
+# _if-else_ Statements
+
+## Introduction
+In this lab you will apply _if-else_ statements to swap and sort cards.  You will also become familiar with an API and how they can be applied to streamline the development process.  
+
+## Your Tasks
+
+- [ ] Locate the "_if-else_ Statements" lab guide [IfElseStatements.pdf](IfElseStatements.pdf)
+
+- [ ] Complete the group portion of the lab as instructed
+
+- [ ] Complete the individual portion of the lab
+
+	* Inside your main website directory, make sure you have created the following directory, 
+		- IfElseStatements
+	* Inside this directory, save the following files
+		- [files/Index.html](files/Index.html) - the html page that will display your program
+		- [files/App.js](files/App.js) - the file you will complete
+		- [files/Card.js](files/Card.js) - the API you will implement
+	* Create a folder called "images" - must be lowercase
+	* Inside the images directory, download all of the images located here
+		- [files/images](files/images)
+
+### Challenge 1
+
+- [ ] Implement all the TODOs as instructed in the App.js file above
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ labs/JavaScript/IfElseStatements/files/App.js
@@ -0,0 +1,90 @@
+/**
+ * Defines the width and height
+ * which will be used to position the buttons
+ */
+var width = Card.width;
+var height = Card.height;
+
+/**
+ * Creates a new random card from 
+ * a standard 52 card deck
+ * TODO: Create two more cards called "b" and "c"
+ */
+var a = new Card();
+
+/**
+ * Creates a button with the word "swap" 
+ * and appends it to the page
+ * TODO:  Add an event listender to the button and call
+ * the appropriate function
+ */
+var swapButton = document.createElement("button");
+swapButton.innerHTML = "swap";
+document.body.append(swapButton);
+
+/**
+ * Creates a button with the word "sort" 
+ * and appends it to the page
+ * TODO:  Add an event listender to the button and call
+ * the appropriate function
+ */
+var sortButton = document.createElement("button");
+sortButton.innerHTML = "sort";
+sortButton.style.position = "absolute";
+sortButton.style.left = width*2 + "px";
+document.body.append(sortButton);
+
+/**
+ * Displays two cards as buttons on the page
+ * TODO:  Get card "b" to appear next to card "a"
+ */
+function showTwoCards(){
+    
+    var h = height;
+    //Make card button
+    var cardButton1 = a.makeCardButton();
+
+    //append card button
+    document.body.append(cardButton1);
+
+    //position card button
+    cardButton1.style.top = height + "px";
+    cardButton1.style.left = width/2 + width*0 + "px";
+}
+
+/**
+ * Displays three cards as buttons on the page.
+ * TODO:  Get cards "a", "b", and "c" to appear when this
+ * function is called
+ */
+function showThreeCards(){
+    
+
+}
+
+/**
+ * Swaps cards a and b and displays the result 
+ * TODO:  Complete the swap function
+ */
+function swap(){
+    
+    //Calls to display the swapped cards
+    showTwoCards();
+    showThreeCards();
+}
+
+/**
+ * Sorts the cards and displays the result 
+ * TODO:  Complete the sort function
+ */
+function sort(){
+
+    //Calls to display the sorted cards
+    showTwoCards();
+    showThreeCards();
+
+}
+
+//Calls to display the cards
+showTwoCards();
+showThreeCards();
