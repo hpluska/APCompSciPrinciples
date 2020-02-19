09c02221b3fe266601825f64697da27afb14b036
hpluska <17304761+hpluska@users.noreply.github.com>
2020-02-19 10:04:11 -0700

    adds

---
+++ labs/JavaScript/WhileLoops/README.md
@@ -0,0 +1,45 @@
+# _while loops_ 
+
+## Introduction
+In this lab you will apply _while loops_ to convert numbers and create interesting shapes on a grid of buttons.   
+
+## Your Tasks
+
+- [ ] Locate the "_while loops_ " lab guide [WhileLoops.pdf](WhileLoops.pdf)
+
+- [ ] Complete the group portion of the lab as instructed
+
+- [ ] Complete the individual portion of the lab
+
+	* Inside your main website directory, make sure you have created the following directory, 
+		- WhileLoops
+	* Inside this directory, save the following files
+		- [files/Index.html](files/Index.html) - the html page that will display your program
+		- [files/App.js](files/App.js) - the file you will complete
+
+### Challenge 1
+
+- [ ] Implement the coin flipping simulator that will keep track of how many coin flips it takes to acheive a specified streak of heads.   For example, how many coin flips does it take to get 3 heads in a row, 5 heads in a row, or even 12 heads in a row?
+
+### Challenge 2
+
+- [ ] Implement a program that prompts the user for a number base (2 thru 9) and a corresponding number, then converts the number to decimal
+
+### Challenge 3
+
+- [ ] Implement the TODOS inthe App.js file provided
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ labs/JavaScript/WhileLoops/files/App.js
@@ -0,0 +1,88 @@
+var height = 50;
+var width = 50;
+var gridSize = 10;
+var rows = 0, cols = 0;
+
+
+makeGrid();
+
+/**
+ * Creates a button and appends it to the page
+ * @param cols column location of button
+ * @param rows row location of button
+ */
+function makeButton(cols, rows){
+
+    b = document.createElement("button");
+    b.style.position = "absolute";
+    b.style.width = width + "px";
+    b.style.height = height + "px";
+    b.style.top = height*rows + "px";
+    b.style.left = width*cols + "px";
+    b.id = rows+""+cols;
+    document.body.append(b);
+    return b;
+}
+
+/**
+ * Creates a grid of buttons
+ */
+function makeGrid(){
+
+    while(rows <= gridSize){
+        while(cols <= gridSize){
+            var but = makeButton(cols, rows); 
+	    b.style.backgroundColor = "RED";
+            cols++;
+        }
+    cols = 0;
+    rows++;
+    }
+}
+
+/**
+ * TODO: Complete the verticalStripes function
+ */
+function verticalStripes(){
+
+}
+
+/**
+ * TODO: Complete the horizontalStripes function
+ */
+function horizontalStripes(){
+
+}
+
+/**
+ * TODO: Complete the checkerBoard function
+ */
+function checkerBoard(){
+
+}
+
+
+/**
+ * TODO:  Complete the stairDown function
+ */
+function stairDown(){
+
+}
+
+
+/**
+ * TODO: Complete the stairUp function
+ */
+function stairUp(){
+
+}
+
+/**
+ * TODO: Complete the stairDown2 function
+ */
+function stairDown2(){
+
+}
+
+
+
