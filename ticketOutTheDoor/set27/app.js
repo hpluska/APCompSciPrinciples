8eac0ead2f1b35d9090be468235e2b7cbfdc1d00
hpluska <17304761+hpluska@users.noreply.github.com>
2021-02-17 06:30:03 -0700

    adds

---
+++ ticketOutTheDoor/set27/app.js
@@ -0,0 +1,54 @@
+var timer;
+//delayTimer();
+
+function delayTimer() {
+  timer = setTimeout(delayedFunction, 3000);
+}
+
+function delayedFunction() {
+  alert("Three seconds have elapsed.");
+}
+
+var timer2;
+//repeatEverySecond();
+
+function repeatEverySecond() {
+  timer2 = setInterval(sendMessage, 1000);
+}
+
+function sendMessage() {
+  var d = new Date();
+  document.body.innerHTML = d.toLocaleTimeString();
+}
+
+var timeoutID;
+//delayTimer();
+
+function delayTimer() {
+  timeoutID = setTimeout(delayedFunction, 3000);
+}
+
+function delayedFunction() {
+  alert("Three seconds have elapsed.");
+  clearAlert();
+}
+
+function clearAlert() {
+  clearTimeout(timeoutID);
+}
+
+var gameTimer;
+var timeLeft;
+var score = 10;
+var gameTime = 15;
+countDown();
+function gameTimer(){
+     timeOut = setTimeout(gameOver, gameTime*1000);
+}
+function countDown() {
+     timeLeft = setInterval(updateTime, 1000);
+}
+function updateTime(){
+     gameTime--;
+     document.body.innerHTML = gameTime;
+}
