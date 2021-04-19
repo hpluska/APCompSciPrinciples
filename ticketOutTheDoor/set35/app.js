c9b2c037b8753aee4abf8f618334b6af663cbee1
hpluska <17304761+hpluska@users.noreply.github.com>
2021-04-19 15:08:50 -0600

    adds

--- ticketOutTheDoor/set35/app.js
+++ ticketOutTheDoor/set35/app.js
@@ -1,5 +1,5 @@
 
-var values = [1, 2, 3, 4, 5, 8, 8, 8]; 
+var values = [2, 7, 11, 12, 12, 13, 13, 13]; 
 var target = 8;   
 
 console.log(binarySearch(values, target));
@@ -8,8 +8,10 @@ function binarySearch(data, target) {
   
     var start = 0;   
     var end = data.length - 1;   
+    var count = 0;
     
-    while (start <= end)   {     
+    while (start <= end)   {    
+     count++;
          var mid = Math.floor((start + end) / 2);     /* Calculate midpoint */   
       
          if (target < data[mid])     {       
@@ -20,6 +22,6 @@ function binarySearch(data, target) {
               return mid;     
          }   
     }   
-    return -1; 
+    return count; 
 }
     
