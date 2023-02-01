a47ac7bab1e3fd7fa536ffe266f8c7f187ff1948
hpluska <17304761+hpluska@users.noreply.github.com>
2023-02-01 10:04:49 -0700

    adds

--- ticketOutTheDoor/set25/app.js
+++ ticketOutTheDoor/set25/app.js
@@ -1,88 +1,88 @@
-var mm = "Hello";
-var nx = "good buddy";
-var c = mm + nx;
-console.log //prints Hellogood buddy... notice no space between o & g
+// var mm = "Hello";
+// var nx = "good buddy";
+// var c = mm + nx;
+// console.log //prints Hellogood buddy... notice no space between o & g
 
-var mm = "Hello";
-var nx = "good buddy";
-console.log(mm+ " " + nx);//prints Hello good buddy..notice the space
-console.log("Hello" + " good buddy"); //prints Hello good buddy
+// var mm = "Hello";
+// var nx = "good buddy";
+// console.log(mm+ " " + nx);//prints Hello good buddy..notice the space
+// console.log("Hello" + " good buddy"); //prints Hello good buddy
 
-var x = 17;
-var s = "Was haben wir gemacht?"//German for "What have we done"
-var combo = s + "" + x;
-console.log(combo);//prints Was haben wir gemacht?17
+// var x = 17;
+// var s = "Was haben wir gemacht?"//German for "What have we done"
+// var combo = s + "" + x;
+// console.log(combo);//prints Was haben wir gemacht?17
 
 var theName = "Donald Duck";
 var len = theName.length;
 console.log(len);
 
-var myPet = "Sparky the dog";
-var smallPart = myPet.substring(4);
-console.log(smallPart);
+// var myPet = "Sparky the dog";
+// var smallPart = myPet.substring(4);
+// console.log(smallPart);
 
-var smallPart2 = myPet.substring(4,12);
-console.log(smallPart2);
+// var smallPart2 = myPet.substring(4,12);
+// console.log(smallPart2);
 
-var phrase ="Where is my car?";
-var phraseLower = phrase.toLowerCase();
+// var phrase ="Where is my car?";
+// var phraseLower = phrase.toLowerCase();
 
-console.log(phraseLower);
-console.log(phrase.toUpperCase());
+// console.log(phraseLower);
+// console.log(phrase.toUpperCase());
 
-console.log("What is the \"right\" way?");
+// console.log("What is the \"right\" way?");
 
-var msg = "Here is one line \n and here is another";
-console.log(msg);
+// var msg = "Here is one line \n and here is another";
+// console.log(msg);
 
-var msg2 = "Everything\t\tis\t\ttabbed";
-console.log(msg2);
+// var msg2 = "Everything\t\tis\t\ttabbed";
+// console.log(msg2);
 
-var path = "C:\\SomeFile.pdf";
-console.log(path);
+// var path = "C:\\SomeFile.pdf";
+// console.log(path);
 
-var somePhrase = "Sparky the dog";
-var firstLetter = somePhrase.charAt(0);
-var lastLetter = somePhrase.charAt(somePhrase.length-1);
-console.log("The first letter is " + firstLetter + ".  The last letter is " + lastLetter + ".");
+// var somePhrase = "Sparky the dog";
+// var firstLetter = somePhrase.charAt(0);
+// var lastLetter = somePhrase.charAt(somePhrase.length-1);
+// console.log("The first letter is " + firstLetter + ".  The last letter is " + lastLetter + ".");
 
-var someText = "Take a Hike!";
-someText = "\t\t" + someText + "\n";//add some space before and after
-console.log("X" + someText.trim() + "X"); //prints XTake a HikeX
+// var someText = "Take a Hike!";
+// someText = "\t\t" + someText + "\n";//add some space before and after
+// console.log("X" + someText.trim() + "X"); //prints XTake a HikeX
 
-var someString = "This is a String!";
-var someString2 = 'Here is another String';
-var someString3 = "100";
+// var someString = "This is a String!";
+// var someString2 = 'Here is another String';
+// var someString3 = "100";
 
-var s = "\t\tLucky hocky puck\t\t";
-var j = 6, z = 99;
+// var s = "\t\tLucky hocky puck\t\t";
+// var j = 6, z = 99;
 
-var p = s.charAt(s.length - 1);
-p += s.charAt(s.length-2);
-p += s.charAt(s.length-3);
-p += s.charAt(s.length-4);
-p += s.charAt(s.length-5);
-console.log(p);
+// var p = s.charAt(s.length - 1);
+// p += s.charAt(s.length-2);
+// p += s.charAt(s.length-3);
+// p += s.charAt(s.length-4);
+// p += s.charAt(s.length-5);
+// console.log(p);
 
-var p1 = s.charAt(j);
-console.log(p1);
-var p2 = s.charAt(z - 90);
-console.log(p2);
+// var p1 = s.charAt(j);
+// console.log(p1);
+// var p2 = s.charAt(z - 90);
+// console.log(p2);
 
-var str = s.trim();
-console.log("+"+str+"+");
+// var str = s.trim();
+// console.log("+"+str+"+");
 
-console.log("A\t\tB\n\C\t\tD\n\E\t\tF\nG\t\tH\nI\t\tJ\nK\t\tL\nM\t\tN\nO\t\tP\nQ\t\tR\nS\t\tT\nU\t\tV\nW\t\tX\nY\t\tZ");
+// console.log("A\t\tB\n\C\t\tD\n\E\t\tF\nG\t\tH\nI\t\tJ\nK\t\tL\nM\t\tN\nO\t\tP\nQ\t\tR\nS\t\tT\nU\t\tV\nW\t\tX\nY\t\tZ");
 
-var myPet = "Sparky the dog";
-var smallPart = myPet.substring(4);
-console.log(smallPart);// prints ky the dog
-var smallPart2 = myPet.substring(4,12);
-console.log(smallPart2);//prints ky the d
-var theName = "Donald Duck";
-var len = theName.length;
-console.log(len);
-var phrase ="Where is my car?";
-var phraseUpper = phrase.toUpperCase();
-console.log(phraseUpper);
+// var myPet = "Sparky the dog";
+// var smallPart = myPet.substring(4);
+// console.log(smallPart);// prints ky the dog
+// var smallPart2 = myPet.substring(4,12);
+// console.log(smallPart2);//prints ky the d
+// var theName = "Donald Duck";
+// var len = theName.length;
+// console.log(len);
+// var phrase ="Where is my car?";
+// var phraseUpper = phrase.toUpperCase();
+// console.log(phraseUpper);
 
--- ticketOutTheDoor/set26/App.js
+++ ticketOutTheDoor/set26/App.js
@@ -1,6 +1,44 @@
-function rectangleArea(width, height){
-    var area = width * height;
-    return area;
-}
+//console.log(true > false)
 
-console.log(rectangleArea(5, 7)); //prints 35
+
+// var x = 10; 
+// var y = 20;
+
+// console.log(x == y);
+
+
+//console.log(false && false);
+//console.log(false || false)
+
+
+var w1 = "fat";
+var w2 = "fad";
+console.log(w1 > w2);
+
+console.log(parseInt(w1.charCodeAt(2)));
+console.log(parseInt(w2.charCodeAt(2)));
+
+// var x = 79; 
+// var y = 46; 
+// var z = -3; 
+// var w = 13.89; 
+// var y2 = 40.0; //y is reassigned
+// var t = true; 
+// var f = false;
+
+// console.log(( x < 10) && (y == 46));//false
+// console.log(( x > 10 ) && ( y == 46 ));//true
+// console.log((( x > 10 ) && ( z != -3 )));//false
+// console.log(( x > 10 ) || ( y == 5 )) ;//true
+// console.log(true && false);//false
+// console.log(true && !false);//true
+//console.log(!t || f);//true
+
+// console.log(x != 3 || f);//true
+// console.log(y/2 > w && w != x);//true
+
+
+//console.log( x > y || c || b && jj%4 != 0);
+
+
+
