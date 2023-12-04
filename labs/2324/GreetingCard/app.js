f02412e7d973ac2fc0c582b683da400bf09f0ba7
hpluska <17304761+hpluska@users.noreply.github.com>
2023-12-04 16:07:19 -0700

    adds

---
+++ labs/2324/GreetingCard/app.js
@@ -0,0 +1,11 @@
+var t = document.createElement("h1");
+t.innerHTML = "Happy Holidays!";
+document.body.append(t);
+
+var hat = document.createElement("div");
+hat.innerHTML = "This is going to be the hat";
+document.body.append(hat);
+
+var head = document.createElement("div");
+head.innerHTML = "This is going to be the head";
+document.body.append(head);
--- ticketOutTheDoor/2324/set21CreatingDOMElements/app.js
+++ ticketOutTheDoor/2324/set21CreatingDOMElements/app.js
@@ -1,16 +1,19 @@
-// var i = document.createElement("img");
-// i.src = "cat.webp";
-// i.alt = "cat butt"
-// document.body.append(i);
-// i.setAttribute("width", "300px");
-// i.setAttribute("height", "200px");
 
-var someElement = document.createElement("div");
-someElement.innerHTML = "My Element!"
-someElement.id = "e1";
-document.body.append(someElement);
+// var anotherParagraph = document.createElement("p");
+// anotherParagraph.innerHTML = "HELLO!";
+// anotherParagraph.id = "p2";
+// document.body.append(anotherParagraph);
 
-var e = document.getElementById("e1");
-e.hidden = false;
+var i = document.createElement("img");
+i.src = "cat.webp";
+i.alt = "cat butt"//optional
+document.body.append(i);
+i.setAttribute("width", "100px");
+
+
+
+
+// var e = document.getElementById("e1");
+// e.hidden = false;
 
 
